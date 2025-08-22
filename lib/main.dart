dc4adcf53bc792bf4259960cd7dd59995369791e
dan120929 <dan.salgado58d@gmail.com>
2025-08-21 23:35:30 -0600

    Release internet permission

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       routes: {
         HomePage.routeName: (context) => const HomePage(),
         SearchByNamePage.routeName: (context) => const SearchByNamePage(),
